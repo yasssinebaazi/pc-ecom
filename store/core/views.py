5de06957606472d9346dfe46ea2f4a02f8175ac5
yasssinebaazi <132118285+yasssinebaazi@users.noreply.github.com>
2023-06-22 15:41:15 +0100

    Add files via upload

---
+++ store/core/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .forms import SignupForm
+from item.models import Category, Item
+from django.contrib.auth import logout
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
+     
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:    
+        form = SignupForm()
+
+    return render(request, 'core/signup.html',{
+        'form': form
+    })
+
+def logout(request):
+    logout(request)
+    
+    return render('core/logout.html')
